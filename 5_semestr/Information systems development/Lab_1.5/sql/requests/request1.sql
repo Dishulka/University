d86a0b349fce4f3cd01c35cf17999165cf3e57c9
Dishulka <dishulka@mail.ru>
2021-10-26 16:48:15 +0300

    Information systems development Lab2 done

--- 5_semestr/Information systems development/Lab_1.5/sql/requests/request1.sql
+++
@@ -1,2 +0,0 @@
-SELECT * FROM orders
-WHERE DateOrder BETWEEN '$Date1' AND '$Date2'
--- 5_semestr/Information systems development/Lab_1.5/sql/requests/request2.sql
+++
@@ -1,4 +0,0 @@
-SELECT orders.idOrder, orders.NameCustomer, orders.contactInfo FROM orders
-LEFT JOIN worksinorder using(idOrder)
-LEFT JOIN services using(idService)
-WHERE services.Name = '$NameService'
