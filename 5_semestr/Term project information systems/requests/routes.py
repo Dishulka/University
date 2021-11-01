b4ac8efa6778925a510e187be8a30b685667e159
Dishulka <dishulka@mail.ru>
2021-11-01 14:47:32 +0300

    Term project information systems
    Complete 50% term project information systems

--- 5_semestr/Term project information systems/requests/routes.py
+++ 5_semestr/Term project information systems/requests/routes.py
@@ -2,55 +2,58 @@ import json
 from flask import Blueprint, render_template, request
 from flask_table import Table, Col
 from sql.SQLmaster import SQLmaster
+from access import group_permission_decorator
 
 requests_app = Blueprint('requests', __name__, template_folder='templates')
 
-SQLserver = SQLmaster(json.load(open('sql/configDataBase.json', 'r')))
+SQLserver = SQLmaster(json.load(open('config/configDataBase.json', 'r')))
 
 
 class ItemTableRequest1(Table):
-    Surname = Col('Surname')
-    Position = Col('Position')
+    Surname = Col('Имя сотрудника')
+    Position = Col('Должность')
 
 
 class ItemTableRequest2(Table):
-    idEmployee = Col('idEmployee')
-    Surname = Col('Surname')
-    Section = Col('Section')
-    HiredDate = Col('HiredDate')
-    FiredDate = Col('FiredDate')
-    Birthday = Col('Birthday')
-    Position = Col('Position')
+    idEmployee = Col('Номер сотрудника')
+    Surname = Col('Имя сотрудника')
+    Section = Col('Отдел сотрудника')
+    HiredDate = Col('Дата приема на работу')
+    FiredDate = Col('Дата увольнения')
+    Birthday = Col('День рождение')
+    Position = Col('Должность')
 
 
 class ItemTableRequest3(Table):
-    idCar = Col('idCar')
-    Brand = Col('Brand')
-    CarYear = Col('CarYear')
-    LicensePlateNumber = Col('LicensePlateNumber')
+    idCar = Col('Номер машины')
+    Brand = Col('Марка')
+    CarYear = Col('Год выпуска')
+    LicensePlateNumber = Col('Лицензионный номер')
 
 
 class ItemTableRequest4(Table):
-    MonthNoteDate = Col('MonthNoteDate')
-    countTTN = Col('countTTN')
+    MonthNoteDate = Col('Месяц оформления ТТН')
+    countTTN = Col('Количество ТТН')
 
 
 class ItemTableRequest5(Table):
-    idClient = Col('idClient')
-    sumWeight = Col('sumWeight')
+    idClient = Col('Номер клиента')
+    sumWeight = Col('Суммарный вес')
 
 
 class ItemTableRequest6(Table):
-    MonthNoteDate = Col('MonthNoteDate')
-    avgWeight = Col('avgWeight')
+    MonthNoteDate = Col('Месяц оформления ТТН')
+    avgWeight = Col('Средний вес')
 
 
 @requests_app.route('/')
+@group_permission_decorator
 def requests_index():
     return render_template('request_index.html')
 
 
 @requests_app.route('/request1', methods=['GET', 'POST'])
+@group_permission_decorator
 def requests_request1():
     if request.method == 'GET':
         return render_template('request_1.html')
@@ -58,17 +61,27 @@ def requests_request1():
         year = request.form.get('year')
         month = request.form.get('month')
         result = SQLserver.request('1_request.sql', year=year, month=month)
+
         table = ItemTableRequest1(result)
+
         HTMLtable = table.__html__()
-        file = open(r'.\requests\templates\request_result_child.html', 'w')
-        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div>')
-        file.write(HTMLtable)
+        file = open(r'.\requests\templates\request_result_child.html', 'w', encoding='utf-8')
+        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div class="article_3">')
+
+        if HTMLtable == "<p>No Items</p>":
+            file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
+        else:
+            file.write(HTMLtable)
+
         file.write('</div>{% endblock %}')
         file.close()
-        return render_template('request_result_child.html')
+
+        return render_template('request_result_child.html', url_back='./request1',
+                               title=f'Сотрудники, которые были наняты в месяце - {month}, в году - {year}')
 
 
 @requests_app.route('/request2', methods=['GET', 'POST'])
+@group_permission_decorator
 def requests_request2():
     if request.method == 'GET':
         return render_template('request_2.html')
@@ -76,16 +89,27 @@ def requests_request2():
         hiredDays = request.form.get('hiredDays')
         result = SQLserver.request('2_request.sql', hiredDays=hiredDays)
         table = ItemTableRequest2(result)
+
         HTMLtable = table.__html__()
-        file = open(r'.\requests\templates\request_result_child.html', 'w')
-        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div>')
-        file.write(HTMLtable)
+
+        file = open(r'.\requests\templates\request_result_child.html', 'w', encoding='utf-8')
+        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div class="article_3">')
+
+
+        if HTMLtable == "<p>No Items</p>":
+            file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
+        else:
+            file.write(HTMLtable)
+
         file.write('</div>{% endblock %}')
         file.close()
-    return render_template('request_result_child.html')
+
+        return render_template('request_result_child.html', url_back='./request2',
+                               title=f'Сотрудники, которых наняли {hiredDays} дней назад')
 
 
 @requests_app.route('/request3', methods=['GET', 'POST'])
+@group_permission_decorator
 def requests_request3():
     if request.method == 'GET':
         return render_template('request_3.html')
@@ -93,22 +117,32 @@ def requests_request3():
         licensePlates = request.form.get('licensePlates')
         result = SQLserver.request('3_request.sql', licensePlates=licensePlates)
         table = ItemTableRequest3(result)
+
         HTMLtable = table.__html__()
-        file = open(r'.\requests\templates\request_result_child.html', 'w')
-        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div>')
-        file.write(HTMLtable)
+
+        file = open(r'.\requests\templates\request_result_child.html', 'w', encoding='utf-8')
+        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div class="article_3">')
+
+        if HTMLtable == "<p>No Items</p>":
+            file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
+        else:
+            file.write(HTMLtable)
         file.write('</div>{% endblock %}')
         file.close()
-    return render_template('request_result_child.html')
+
+        return render_template('request_result_child.html', url_back='./request3',
+                               title=f'Машины, где в лицензионном номере есть буквы: {licensePlates}')
 
 
 @requests_app.route('/request4', methods=['GET', 'POST'])
+@group_permission_decorator
 def requests_request4():
     if request.method == 'GET':
         return render_template('request_4.html')
     else:
         year = request.form.get('year')
         result = SQLserver.request('4_request.sql', year=year)
+
         for temp in result:
             temp['MonthNoteDate'] = temp['MONTH(NoteDate)']
             del temp['MONTH(NoteDate)']
@@ -116,42 +150,61 @@ def requests_request4():
             temp['countTTN'] = temp['COUNT(idConsignmentNote)']
             del temp['COUNT(idConsignmentNote)']
         table = ItemTableRequest4(result)
+
         HTMLtable = table.__html__()
-        file = open(r'.\requests\templates\request_result_child.html', 'w')
-        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div>')
-        file.write(HTMLtable)
+        file = open(r'.\requests\templates\request_result_child.html', 'w', encoding='utf-8')
+        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div class="article_3">')
+
+        if HTMLtable == "<p>No Items</p>":
+            file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
+        else:
+            file.write(HTMLtable)
+
         file.write('</div>{% endblock %}')
         file.close()
-    return render_template('request_result_child.html')
+
+        return render_template('request_result_child.html', url_back='./request4',
+                               title=f'Количество ТТН, которые оформили в {year} году')
 
 
 @requests_app.route('/request5', methods=['GET', 'POST'])
+@group_permission_decorator
 def requests_request5():
     if request.method == 'GET':
         return render_template('request_5.html')
     else:
         year = request.form.get('year')
         result = SQLserver.request('5_request.sql', year=year)
+
         for temp in result:
             temp['sumWeight'] = temp['SUM(Weight)']
             del temp['SUM(Weight)']
         table = ItemTableRequest5(result)
+
         HTMLtable = table.__html__()
-        file = open(r'.\requests\templates\request_result_child.html', 'w')
-        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div>')
-        file.write(HTMLtable)
+        file = open(r'.\requests\templates\request_result_child.html', 'w', encoding='utf-8')
+        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div class="article_3">')
+
+        if HTMLtable == "<p>No Items</p>":
+            file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
+        else:
+            file.write(HTMLtable)
+
         file.write('</div>{% endblock %}')
         file.close()
-    return render_template('request_result_child.html')
+        return render_template('request_result_child.html', url_back='./request5',
+                               title=f'Суммарный вес клиентов за {year} год')
 
 
 @requests_app.route('/request6', methods=['GET', 'POST'])
+@group_permission_decorator
 def requests_request6():
     if request.method == 'GET':
         return render_template('request_6.html')
     else:
         year = request.form.get('year')
         result = SQLserver.request('6_request.sql', year=year)
+
         for temp in result:
             temp['MonthNoteDate'] = temp['MONTH(NoteDate)']
             del temp['MONTH(NoteDate)']
@@ -159,10 +212,17 @@ def requests_request6():
             temp['avgWeight'] = temp['AVG(Weight)']
             del temp['AVG(Weight)']
         table = ItemTableRequest6(result)
+
         HTMLtable = table.__html__()
-        file = open(r'.\requests\templates\request_result_child.html', 'w')
-        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div>')
-        file.write(HTMLtable)
+        file = open(r'.\requests\templates\request_result_child.html', 'w', encoding='utf-8')
+        file.write('{% extends \'request_result_main.html\' %}{% block child %}<div class="article_3">')
+
+        if HTMLtable == "<p>No Items</p>":
+            file.write('<p class="article_2">По данному запросу результатов не найдено</p>')
+        else:
+            file.write(HTMLtable)
+
         file.write('</div>{% endblock %}')
         file.close()
-    return render_template('request_result_child.html')
+    return render_template('request_result_child.html', url_back='./request6',
+                               title=f'Средний вес, который перевезли, по месяцем в {year} году')
